51ac27efef769293942310cb06ced02b928c913e
Rodrigo Braz <orodrigobraz18@gmail.com>
2026-05-23 10:52:31 -0300

    Updates

--- server/assets/start-C-XDGgju.js
+++ server/assets/start-C-XDGgju.js
@@ -1,16 +1,9 @@
-import { r as renderErrorPage } from "../server.js";
-import { c as createMiddleware } from "./server-Bcq-nBJQ.js";
+import { r as renderErrorPage } from "./worker-entry-DQSAjIdl.js";
+import { h as createMiddleware } from "./server-CKrf-D6y.js";
+import "node:events";
 import "node:async_hooks";
-import "h3-v2";
-import "@tanstack/router-core";
-import "seroval";
-import "@tanstack/history";
-import "@tanstack/router-core/ssr/client";
-import "@tanstack/router-core/ssr/server";
-import "react";
-import "@tanstack/react-router";
-import "react/jsx-runtime";
-import "@tanstack/react-router/ssr/server";
+import "node:stream/web";
+import "node:stream";
 function dedupeSerializationAdapters(deduped, serializationAdapters) {
   for (let i = 0, len = serializationAdapters.length; i < len; i++) {
     const current = serializationAdapters[i];
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-DQSAjIdl.js";
+import "node:events";
+export {
+  w as default
+};
