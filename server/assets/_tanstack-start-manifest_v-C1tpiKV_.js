0e07b66f94e6f118ef8c7b2d0fb1f1a00e7837ed
Rodrigo Braz <orodrigobraz18@gmail.com>
2026-05-23 11:12:56 -0300

    Updates

--- server/assets/_tanstack-start-manifest_v-C1tpiKV_.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Computador/OneDrive/Área de Trabalho/Dev/Web/Curso2/Projeto Portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-DEL2CLKj.js"] }, "/": { filePath: "C:/Users/Computador/OneDrive/Área de Trabalho/Dev/Web/Curso2/Projeto Portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-DlqzuEHG.js"] } }, clientEntry: "/assets/index-DEL2CLKj.js" });
-export {
-  tsrStartManifest
-};
--- tanstack_start_app/index.js
+++
@@ -1,5 +0,0 @@
-import { w } from "./assets/worker-entry-DIga3w1D.js";
-import "node:events";
-export {
-  w as default
-};
